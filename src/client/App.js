cd359640546bc55f932effdade92e5aa3e0e3455
Zachery Converse <zacheryconverse@gmail.com>
2021-03-26 16:21:21 -0600

    init commit

---
+++ src/client/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hey-o
+    </div>
+  );
+}
+
+export default App;
---
+++ src/server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { StreamChat } from "stream-chat";
+require("dotenv").config({ path: __dirname + "/.env" });
+
+const key = process.env["REACT_APP_KEY"];
+const secret = process.env["SECRET"];
+const serverClient =  StreamChat.getInstance(key, secret);
+
+const app = express();
+const PORT = 8000;
+
+app.post("/token", (req, res) => {
+  const token = client.createToken(key, secret);
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!!`));
+
+// await client.setGuestUser({ id: "tommaso" });
+// await client.connectAnonymousUser();
+
