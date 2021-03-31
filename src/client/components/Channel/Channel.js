369c6e2f71981a19e78c6e9aecba4d211595ba4a
Cody Smith <monstaro32@gmail.com>
2021-03-31 10:53:39 -0600

    Fixes logic to display newest message

--- src/client/components/Channel/Channel.js
+++ src/client/components/Channel/Channel.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './Channel.css'
 const Channel = ({channelList}) => {
-    const renderChannelItems = () => {
-        return channelList.map(channel => <div className="channel-container">
-            <h2>{channel.id}</h2>
-            <p>{channel.state.messages[0].text}</p>
-        </div>)
+    if (channelList.length) {
+        console.log(channelList)
+        const renderChannelItems = () => {
+            return channelList.map(channel => <div className="channel-container">
+                <h2>{channel.id}</h2>
+                <p>{channel.state.messages[channel.state.messages.length - 1].text}</p>
+            </div>)
+        }
+        return (
+            <div>
+                {renderChannelItems()}
+            </div>
+        )
+    } else {
+        return ('Loading')
     }
-    return (
-        <div>
-            {renderChannelItems()}
-        </div>
-    )
+   
 }
 
 export default Channel;
