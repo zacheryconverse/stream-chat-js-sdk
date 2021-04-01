1e354207e09ec041ee17192c990fcf2476907400
Zachery Converse <zacheryconverse@gmail.com>
2021-04-01 13:36:32 -0600

    scroll to bottom of messageList on load.

--- src/client/components/MessageList.js
+++ src/client/components/MessageList.js
@@ -1,8 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, Fragment } from "react";
 
 export default function MessageList({ chatClient }) {
   const [messages, setMessages] = useState("");
+  const messagesEndRef = useRef(null);
   const channel = chatClient.channel("messaging", "channel-id-123");
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   useEffect(() => {
     const fetchMessages = async () => {
       const response = await channel.watch();
@@ -11,17 +17,13 @@ export default function MessageList({ chatClient }) {
       //   console.log("Start Channel", e);
       //   console.log("Channel State", channel.state.messages);
       // });
+      scrollToBottom();
       channel.on("message.new", (e) => {
         console.log("New Message", e);
         console.log("Channel State", channel.state);
         setMessages(channel.state.messages);
         // change to optimistic render in sendMessage??
       });
-
-      // const response = chatClient.on((e) => {
-      //   console.log("Event", e);
-      //   console.log("Channel State", channel.state);
-      // });
     };
     fetchMessages();
   }, [channel, chatClient]);
@@ -32,7 +34,10 @@ export default function MessageList({ chatClient }) {
       <ul className="messages">
         {messages
           ? messages.map((message, i) => (
-              <li key={message.id}> {message.text + "\n"} </li>
+              <Fragment>
+                <li key={message.id}>{message.text + "\n"}</li>
+                <div ref={messagesEndRef} />
+              </Fragment>
             ))
           : ""}
       </ul>
