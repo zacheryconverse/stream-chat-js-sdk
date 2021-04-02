6553b6388b637c3b66de4ed79a233fce3827f002
Cody Smith <monstaro32@gmail.com>
2021-04-02 09:31:53 -0600

    Fixes dependencies

--- src/client/components/ChannelList/ChannelList.js
+++ src/client/components/ChannelList/ChannelList.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import Channel from "../Channel/Channel";
 import "./ChannelList.css";
@@ -17,9 +18,7 @@ const ChannelList = ({ chatClient, setActiveChannel }) => {
         })
         .then((r) => setChannelList(r));
     },
-    [],
-    filter,
-    sort
+    []
   );
 
   const createChannel = async (e) => {
